43ae5bd95aada50d1d86d548febe535a72117669
bhragavig29 <bhragavi.govardhanama@gmail.com>
2020-06-11 19:52:46 +0530

    Search with all fields

--- src/app/POC/products/pipes/search.pipe.ts
+++ src/app/POC/products/pipes/search.pipe.ts
@@ -5,24 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
   
-transform(items: any[], searchText: string, fieldName: string): any[] {
+// transform(items: any[], searchText: string, fieldName: string): any[] {
 
-    // return empty array if array is falsy
-    if (!items) { return []; }
+//     // return empty array if array is falsy
+//     if (!items) { return []; }
 
-    // return the original array if search text is empty
-    if (!searchText) { return items; }
+//     // return the original array if search text is empty
+//     if (!searchText) { return items; }
 
-    // convert the searchText to lower case
-    searchText = searchText.toLowerCase();
+//     // convert the searchText to lower case
+//     searchText = searchText.toLowerCase();
 
-    // retrun the filtered array
-    return items.filter(item => {
-      if (item && item[fieldName]) {
-        return item[fieldName].toLowerCase().includes(searchText);
-      }
-      return false;
-    });
-   }
+//     // retrun the filtered array
+//     return items.filter(item => {
+//       if (item && item[fieldName]) {
+//         return item[fieldName].toLowerCase().includes(searchText);
+//       }
+//       return false;
+//     });
+//    }
+public transform(value, keys: string, term: string) {
+
+  if (!term) return value;
+  return (value || []).filter(item => keys.split(',').some(key => item.hasOwnProperty(key) && new RegExp(term, 'gi').test(item[key])));
 
 }
+}
+
